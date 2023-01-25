8b52ceb888fd8a092b4d53670d14661affe64a20
jx-dohwan <79912056+jx-dohwan@users.noreply.github.com>
2023-01-25 21:08:26 +0900

    Add files via upload

--- Serving/run.py
+++ Serving/run.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template, request
 from transformers import pipeline
 from preprocessor import preprocess_sentence, preprocess_result
-from time_check import do_something
+#from time_check import do_something
 import math
 import time
 
@@ -14,23 +14,19 @@ gen_kwargs = {"length_penalty": 1.2, "num_beams":8, "max_length": 128}
 @app.route('/', methods=['GET','POST'])
 
 def home():
-    
-    do_something()
+    #do_something()
     text_output = ""
     text_input = str(request.form.get('size'))
-    if text_input:
-        
+    pipe = pipeline("summarization", model=model_name)
+    if text_input: 
         text_input = preprocess_sentence(text_input)
         result = preprocess_result(text_input)
         del result[0]
-        print(result)
-        start = time.time()
-        pipe = pipeline("summarization", model=model_name)
+        #start = time.time()
         
         text_output = pipe('[sep]'.join(result), **gen_kwargs)[0]["summary_text"]
-        end = time.time()
-
-        print("걸린시간",end - start)
+        #end = time.time()
+        #print("걸린시간",end - start)
     return render_template('index.html', text_output=text_output)
 
 if __name__ == '__main__':
